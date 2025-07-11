65f95d68cd1e18731f4c959320b315698ff0005d
Anastasia Kvitka <nasti.carrot@gmail.com>
2025-07-11 14:23:12 +0300

    added projects to main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,14 @@ import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
 import { Navigation } from "@/components/Navigation";
 import { ScrollToTop } from "@/components/ScrollToTop";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <>
       <Navigation />
       <Hero />
+      <Projects />
       <About />
       <Grant />
       <HowItWorks />
